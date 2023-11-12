5bbca6b227d559f229370bfa8dc8bd25fc44066a
FoVNull <JKL4131@126.com>
2023-11-12 15:18:36 +0900

    disable en moel for webui

--- webui.py
+++ webui.py
@@ -212,7 +212,7 @@ if __name__ == "__main__":
     )
     speaker_ids = hps.data.spk2id
     speakers = list(speaker_ids.keys())
-    languages = ["ZH", "JP", "EN", "mix", "auto"]
+    languages = ["ZH", "JP", "mix"] # disable auto / EN
     with gr.Blocks() as app:
         with gr.Row():
             with gr.Column():
